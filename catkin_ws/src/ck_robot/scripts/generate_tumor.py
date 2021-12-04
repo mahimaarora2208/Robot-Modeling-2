5840618a682c489c9bae997208cf11863e44ccf7
Tyler <tylerbarrett2010+github@gmail.com>
2021-12-04 17:41:10 -0500

    Better Generate_Tumor
    The tumor is now automatically generated in Gazebo, or moved to a new position if it already exists.

--- catkin_ws/src/ck_robot/scripts/generate_tumor.py
+++ catkin_ws/src/ck_robot/scripts/generate_tumor.py
@@ -2,24 +2,68 @@
 
 import math
 import random
+import os
 
 import rospy
 from geometry_msgs.msg import PoseStamped
 
+from gazebo_msgs.msg import ModelStates, ModelState
+from gazebo_msgs.srv import SetModelState
+
+
+def spawn_tumor(x, y, z):
+    spawn_command = "rosrun gazebo_ros spawn_model -file urdf/sphere.urdf -urdf -model tumor"
+    spawn_command += " -x " + str(x)
+    spawn_command += " -y " + str(y)
+    spawn_command += " -z " + str(z)
+
+    # os.system("source ~/.bashrc && roscd ck_robot && " + spawn_command)
+    os.system(spawn_command)
+
+def move_tumor(x, y, z):
+    new_tumor_state = ModelState()
+    new_tumor_state.model_name = "tumor"
+    new_tumor_state.pose.position.x = x
+    new_tumor_state.pose.position.y = y
+    new_tumor_state.pose.position.z = z
+
+    print("Moving tumor to " + str(x) + ", " + str(y) + ", " + str(z))
+    rospy.wait_for_service('/gazebo/set_model_state')
+    set_state = rospy.ServiceProxy("/gazebo/set_model_state", SetModelState)
+    set_state(new_tumor_state)
+
+
 def init_tumor_generator():
-    tumor_pub = rospy.Publisher('tumor_location', PoseStamped, queue_size=10)
     rospy.init_node('generate_tumor', anonymous=True)
 
-    tumor_distance = 10 * random.random()
+    # Generate tumor coordinates
+    tumor_distance = random.random() + 0.5
     tumor_angle = 2 * math.pi * random.random()
-    tumor_elevation = 10 * random.random()
+    tumor_elevation = random.random() + 0.5
 
+    x = tumor_distance * math.cos(tumor_angle) 
+    y = tumor_distance * math.sin(tumor_angle) 
+    z = tumor_elevation
+
+    # Wait for a list of models currently in Gazebo, and spawn/move tumor accordingly
+    model_states = rospy.wait_for_message("/gazebo/model_states", ModelStates, timeout=None)
+    model_names = model_states.name
+    print("Model names: " + str(model_names))
+
+    if "tumor" not in model_names:
+        spawn_tumor(x, y, z)
+    else:
+        move_tumor(x, y, z)
+
+    # Publish our coordinates to our own topic
+    tumor_pub = rospy.Publisher('tumor_location', PoseStamped, queue_size=10)
     tumor_location = PoseStamped()
-    tumor_location.pose.position.x = tumor_distance * math.cos(tumor_angle) 
-    tumor_location.pose.position.y = tumor_distance * math.sin(tumor_angle) 
-    tumor_location.pose.position.z = tumor_elevation
+    tumor_location.pose.position.x = x
+    tumor_location.pose.position.y = y
+    tumor_location.pose.position.z = z
 
     tumor_pub.publish(tumor_location)
+    
 
 if __name__ == '__main__':
     try:
